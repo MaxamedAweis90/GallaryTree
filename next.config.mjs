a83f8ca3102dd7bae65fecb458b3febf1b63abab
unknown <mohamedaweys90@gmail.com>
2025-03-11 15:35:47 +0300

    Your commit message

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // This enables static export mode
-    trailingSlash: true, // Optional: ensures paths end with a trailing slash (good for static hosting)
-  };
-  
-  export default nextConfig;
-  
+  output: "standalone",
+};
+
+module.exports = nextConfig;
